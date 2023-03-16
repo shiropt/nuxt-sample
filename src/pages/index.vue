53302cc6515e8c441eb052b9d08778cba868d0d7
shiropt <shiro19880610@yahoo.co.jp>
2023-03-16 22:24:06 +0900

    inital commit

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="countUp">Count UP</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Index extends Vue {
+  public count = 0
+  public countUp() {
+    this.count++
+  }
+}
+</script>
+<style scoped></style>
